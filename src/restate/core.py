743fa33040654b6d4e468608906c816c9f2bd063
Yibo Wei <david_wyb2001@outlook.com>
2024-11-24 18:58:46 -0800

    Refactor Middleware to take a store instead of state

--- src/restate/core.py
+++ src/restate/core.py
@@ -4,28 +4,30 @@ from typing import Any
 
 
 @dataclass
-class Middleware[S, A]:
+class Middleware[A]:
     """Middleware class to add side effects to the store."""
 
-    before_reduce: Callable[[S, A], A | None] | None = None
-    after_reduce: Callable[[S, A], None] | None = None
+    before_reduce: Callable[["Store", A], A | None] | None = None
+    """Function to run before the reducer."""
+    after_reduce: Callable[["Store", A], None] | None = None
+    """Function to run after the reducer."""
 
 
-class Store[S, A]:
+class Store[T, A]:
     """A single source of truth for the application state."""
 
-    def __init__(self, reducer: Callable[[S, A], S], initial_state: S):
-        self._reducer = reducer
+    def __init__(self, reducer: Callable[[T, A], T], initial_state: T):
+        self.reducer = reducer
         self._state = initial_state
-        self._subscribers: list[Callable[[S], Any]] = []
-        self._middlewares: list[Middleware[S, A]] = []
+        self._subscribers: list[Callable[[T], Any]] = []
+        self._middlewares: list[Middleware[A]] = []
 
     @property
-    def state(self) -> S:
+    def state(self) -> T:
         """Expose read-only state property."""
         return self._state
 
-    def get_state(self) -> S:
+    def get_state(self) -> T:
         """Get the current state."""
         return self._state
 
@@ -35,26 +37,28 @@ class Store[S, A]:
             if middleware.before_reduce:
                 if not action:
                     return
-                action = middleware.before_reduce(self._state, action)
+                action = middleware.before_reduce(self, action)
 
         if not action:
             return
 
-        self._state = self._reducer(self.state, action)
+        new_state = self.reducer(self.state, action)
 
         for middleware in self._middlewares:
             if middleware.after_reduce:
-                middleware.after_reduce(self._state, action)
+                middleware.after_reduce(self, action)
+
+        self._state = new_state
 
         for subscriber in self._subscribers:
             subscriber(self._state)
 
-    def subscribe(self, subscriber: Callable[[S], Any]) -> Callable[[], None]:
+    def subscribe(self, subscriber: Callable[[T], Any]) -> Callable[[], None]:
         """Subscribe to state changes."""
         self._subscribers.append(subscriber)
         return lambda: self._subscribers.remove(subscriber)
 
-    def add_middleware(self, *middlewares: Middleware[S, A]) -> None:
+    def add_middleware(self, *middlewares: Middleware[A]) -> None:
         """Add middlewares to the store."""
         for m in middlewares:
             self._middlewares.append(m)
